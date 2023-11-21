3aee2a01ab28914acd66ba3bc636ebc362d3d6dc
BadyTM <70290226+BadyTM@users.noreply.github.com>
2023-11-21 20:55:39 +0100

    Update time calculator.js

--- time calculator.js
+++ time calculator.js
@@ -1,14 +1,3 @@
-/*
- * Script Name: Time divider
- * Version: 1
- * Last Updated: 21-11-2023
- * Author: BadyTM
- * Author Contact: N/A
- * Approved Date: N/A
- */
-
-/*--------------------------------------------------------------------------------------
- * This script can NOT be cloned and modified without permission from the script author.
- --------------------------------------------------------------------------------------*/
+javascript:
 
 console.log("wwwwww);
