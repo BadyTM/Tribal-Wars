898288725fb46b81174ebc716aee94eb3a83c141
BadyTM <70290226+BadyTM@users.noreply.github.com>
2024-09-28 12:59:40 +0200

    Adding tsconfig and script file

---
+++ src/eventBot.js
@@ -0,0 +1,4 @@
+const enemyUnitZone = document.querySelector(".enemy-unit-zone");
+const enemyUnits = enemyUnitZone?.querySelectorAll(".trump-unit");
+console.log(enemyUnits);
+export {};
---
+++ src/eventBot.ts
@@ -0,0 +1,3 @@
+const enemyUnitZone = document.querySelector(".enemy-unit-zone");
+const enemyUnits = enemyUnitZone?.querySelectorAll(".trump-unit");
+console.log(enemyUnits);
