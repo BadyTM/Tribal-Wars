44cca95181aa6d069734880d130333240011a41a
BadyTM <70290226+BadyTM@users.noreply.github.com>
2024-09-28 23:26:36 +0200

    Adding functionality to get units with stats in order

--- src/eventBot.js
+++ src/eventBot.js
@@ -1,25 +1,43 @@
-const enemyUnitZone = document.querySelector(".enemy-unit-zone");
-const enemyUnitsElements = [...enemyUnitZone.querySelectorAll(".trump-unit")];
-const enemyUnits = [];
-const ownUnitZone = document.querySelector(".own-unit-zone");
-const ownUnitsElements = [...ownUnitZone.querySelectorAll(".trump-unit")];
-const ownUnits = [];
+const enemyUnitWrapper = document.querySelector(".enemy-unit-zone");
+const enemyUnitsElements = [...enemyUnitWrapper.querySelectorAll(".trump-unit")];
+const enemyUnitsWithStats = [];
+const ownUnitWrapper = document.querySelector(".own-unit-zone");
+const ownUnitsElements = [...ownUnitWrapper.querySelectorAll(".trump-unit")];
+const ownUnitsWithStats = [];
+const unitsInOrder = [];
 const getUnitsStats = (unitsElementsArray, unitsStatsArray) => {
-    unitsElementsArray?.forEach((unit) => {
-        const health = parseInt(unit.querySelector(".health").innerText);
-        const attackWrapper = unit.querySelector(".attack");
-        const attackMin = parseInt((attackWrapper?.querySelector(".min")).innerText);
-        const attackMax = parseInt((attackWrapper?.querySelector(".max")).innerText);
+    unitsElementsArray?.forEach((unitElement) => {
+        const unitClasses = unitElement.querySelector(".trump-unit-portrait")?.classList;
+        const unitClassName = [...unitClasses].find((className) => className.startsWith("look-"));
+        const unitHealth = parseInt(unitElement.querySelector(".health").innerText);
+        const unitAttackWrapper = unitElement.querySelector(".attack");
+        const unitAttackMin = parseInt((unitAttackWrapper?.querySelector(".min")).innerText);
+        const unitAttackMax = parseInt((unitAttackWrapper?.querySelector(".max")).innerText);
         const unitWithStats = {
-            unitElement: unit,
-            health: health,
-            attackMin: attackMin,
-            attackMax: attackMax,
+            unitName: unitClassName,
+            unitElement: unitElement,
+            health: unitHealth,
+            attackMin: unitAttackMin,
+            attackMax: unitAttackMax,
+            attackAvg: (unitAttackMax + unitAttackMin) / 2,
         };
         unitsStatsArray.push(unitWithStats);
     });
-    console.log(unitsStatsArray);
 };
-getUnitsStats(enemyUnitsElements, enemyUnits);
-getUnitsStats(ownUnitsElements, ownUnits);
+const getUnitsOrder = () => {
+    const orderWrapper = document.querySelector(".turn-order");
+    const unitsElementsInOrder = [...orderWrapper.querySelectorAll(".trump-unit-portrait")];
+    unitsElementsInOrder?.forEach((unitElement) => {
+        const unitClassName = [...unitElement.classList].find((className) => className.startsWith("look-"));
+        unitsInOrder.push(unitClassName);
+    });
+    const allUnits = [...enemyUnitsWithStats, ...ownUnitsWithStats];
+    allUnits.sort((a, b) => {
+        return unitsInOrder.indexOf(a.unitName) - unitsInOrder.indexOf(b.unitName);
+    });
+    return allUnits;
+};
+getUnitsStats(enemyUnitsElements, enemyUnitsWithStats);
+getUnitsStats(ownUnitsElements, ownUnitsWithStats);
+console.log(getUnitsOrder());
 export {};
--- src/eventBot.ts
+++ src/eventBot.ts
@@ -1,28 +1,62 @@
-const enemyUnitZone = document.querySelector(".enemy-unit-zone") as HTMLElement;
-const enemyUnitsElements = [...enemyUnitZone.querySelectorAll(".trump-unit")] as HTMLElement[];
-const enemyUnits: Record<string, number | HTMLElement>[] = [];
+interface UnitWithStats {
+  unitName: string;
+  unitElement: HTMLElement;
+  health: number;
+  attackMin: number;
+  attackMax: number;
+  attackAvg: number;
+}
 
-const ownUnitZone = document.querySelector(".own-unit-zone") as HTMLElement;
-const ownUnitsElements = [...ownUnitZone.querySelectorAll(".trump-unit")] as HTMLElement[];
-const ownUnits: Record<string, number>[] = [];
+const enemyUnitWrapper = document.querySelector(".enemy-unit-zone") as HTMLElement;
+const enemyUnitsElements = [...enemyUnitWrapper.querySelectorAll(".trump-unit")] as HTMLElement[];
+const enemyUnitsWithStats: UnitWithStats[] = [];
 
-const getUnitsStats = (unitsElementsArray: HTMLElement[], unitsStatsArray: object[]) => {
-  unitsElementsArray?.forEach((unit: HTMLElement): void => {
-    const health = parseInt((unit.querySelector(".health") as HTMLElement).innerText);
-    const attackWrapper = unit.querySelector(".attack");
-    const attackMin = parseInt((attackWrapper?.querySelector(".min") as HTMLElement).innerText);
-    const attackMax = parseInt((attackWrapper?.querySelector(".max") as HTMLElement).innerText);
-    const unitWithStats = {
-      unitElement: unit,
-      health: health,
-      attackMin: attackMin,
-      attackMax: attackMax,
+const ownUnitWrapper = document.querySelector(".own-unit-zone") as HTMLElement;
+const ownUnitsElements = [...ownUnitWrapper.querySelectorAll(".trump-unit")] as HTMLElement[];
+const ownUnitsWithStats: UnitWithStats[] = [];
+
+const unitsInOrder: string[] = [];
+
+const getUnitsStats = (unitsElementsArray: HTMLElement[], unitsStatsArray: UnitWithStats[]): void => {
+  unitsElementsArray?.forEach((unitElement: HTMLElement): void => {
+    const unitClasses = unitElement.querySelector(".trump-unit-portrait")?.classList as DOMTokenList;
+
+    const unitClassName = [...unitClasses].find((className) => className.startsWith("look-")) as string;
+    const unitHealth = parseInt((unitElement.querySelector(".health") as HTMLElement).innerText);
+    const unitAttackWrapper = unitElement.querySelector(".attack");
+    const unitAttackMin = parseInt((unitAttackWrapper?.querySelector(".min") as HTMLElement).innerText);
+    const unitAttackMax = parseInt((unitAttackWrapper?.querySelector(".max") as HTMLElement).innerText);
+
+    const unitWithStats: UnitWithStats = {
+      unitName: unitClassName,
+      unitElement: unitElement,
+      health: unitHealth,
+      attackMin: unitAttackMin,
+      attackMax: unitAttackMax,
+      attackAvg: (unitAttackMax + unitAttackMin) / 2,
     };
 
     unitsStatsArray.push(unitWithStats);
   });
-  console.log(unitsStatsArray);
 };
 
-getUnitsStats(enemyUnitsElements, enemyUnits);
-getUnitsStats(ownUnitsElements, ownUnits);
+const getUnitsOrder = (): UnitWithStats[] => {
+  const orderWrapper = document.querySelector(".turn-order") as HTMLElement;
+  const unitsElementsInOrder = [...orderWrapper.querySelectorAll(".trump-unit-portrait")] as HTMLElement[];
+
+  unitsElementsInOrder?.forEach((unitElement: HTMLElement) => {
+    const unitClassName = [...unitElement.classList].find((className) => className.startsWith("look-")) as string;
+    unitsInOrder.push(unitClassName);
+  });
+
+  const allUnits = [...enemyUnitsWithStats, ...ownUnitsWithStats];
+  allUnits.sort((a, b) => {
+    return unitsInOrder.indexOf(a.unitName) - unitsInOrder.indexOf(b.unitName);
+  });
+
+  return allUnits;
+};
+
+getUnitsStats(enemyUnitsElements, enemyUnitsWithStats);
+getUnitsStats(ownUnitsElements, ownUnitsWithStats);
+console.log(getUnitsOrder());
