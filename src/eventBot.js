d90d1eb1d1eaa19c5aa1c2c5f9c66138532cdbef
BadyTM <70290226+BadyTM@users.noreply.github.com>
2024-09-28 13:42:55 +0200

    Getting info about units

--- src/eventBot.js
+++ src/eventBot.js
@@ -1,4 +1,25 @@
 const enemyUnitZone = document.querySelector(".enemy-unit-zone");
-const enemyUnits = enemyUnitZone?.querySelectorAll(".trump-unit");
-console.log(enemyUnits);
+const enemyUnitsElements = [...enemyUnitZone.querySelectorAll(".trump-unit")];
+const enemyUnits = [];
+const ownUnitZone = document.querySelector(".own-unit-zone");
+const ownUnitsElements = [...ownUnitZone.querySelectorAll(".trump-unit")];
+const ownUnits = [];
+const getUnitsStats = (unitsElementsArray, unitsStatsArray) => {
+    unitsElementsArray?.forEach((unit) => {
+        const health = parseInt(unit.querySelector(".health").innerText);
+        const attackWrapper = unit.querySelector(".attack");
+        const attackMin = parseInt((attackWrapper?.querySelector(".min")).innerText);
+        const attackMax = parseInt((attackWrapper?.querySelector(".max")).innerText);
+        const unitWithStats = {
+            unitElement: unit,
+            health: health,
+            attackMin: attackMin,
+            attackMax: attackMax,
+        };
+        unitsStatsArray.push(unitWithStats);
+    });
+    console.log(unitsStatsArray);
+};
+getUnitsStats(enemyUnitsElements, enemyUnits);
+getUnitsStats(ownUnitsElements, ownUnits);
 export {};
--- src/eventBot.ts
+++ src/eventBot.ts
@@ -1,3 +1,28 @@
-const enemyUnitZone = document.querySelector(".enemy-unit-zone");
-const enemyUnits = enemyUnitZone?.querySelectorAll(".trump-unit");
-console.log(enemyUnits);
+const enemyUnitZone = document.querySelector(".enemy-unit-zone") as HTMLElement;
+const enemyUnitsElements = [...enemyUnitZone.querySelectorAll(".trump-unit")] as HTMLElement[];
+const enemyUnits: Record<string, number | HTMLElement>[] = [];
+
+const ownUnitZone = document.querySelector(".own-unit-zone") as HTMLElement;
+const ownUnitsElements = [...ownUnitZone.querySelectorAll(".trump-unit")] as HTMLElement[];
+const ownUnits: Record<string, number>[] = [];
+
+const getUnitsStats = (unitsElementsArray: HTMLElement[], unitsStatsArray: object[]) => {
+  unitsElementsArray?.forEach((unit: HTMLElement): void => {
+    const health = parseInt((unit.querySelector(".health") as HTMLElement).innerText);
+    const attackWrapper = unit.querySelector(".attack");
+    const attackMin = parseInt((attackWrapper?.querySelector(".min") as HTMLElement).innerText);
+    const attackMax = parseInt((attackWrapper?.querySelector(".max") as HTMLElement).innerText);
+    const unitWithStats = {
+      unitElement: unit,
+      health: health,
+      attackMin: attackMin,
+      attackMax: attackMax,
+    };
+
+    unitsStatsArray.push(unitWithStats);
+  });
+  console.log(unitsStatsArray);
+};
+
+getUnitsStats(enemyUnitsElements, enemyUnits);
+getUnitsStats(ownUnitsElements, ownUnits);
